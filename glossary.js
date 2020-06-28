895752e916189b16d0fc9fa3f60169f345814415
Toshiyuki Masui <masui@pitecan.com>
2020-06-28 10:29:32 +0900

    dataをとりあえず追加

---
+++ glossary.js
@@ -0,0 +1,15 @@
+//
+// これをScrapboxから生成したい
+//
+var glossary = {
+    'change': '変更|修正|編集',
+    'display': '表示する|見る|リストする',
+    'del': '消す|消去する|削除する',
+    'modified': '(変更|修正|編集)(された|があった)' // このカッコはまずい
+};
+
+module.exports = glossary;
+
+//var xxx = 100;
+//var yyy = 200;
+//module.exports = {xxx, yyy};
