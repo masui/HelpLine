a077f5e86000149790fddda1def7667c6f23b155
Toshiyuki Masui <masui@pitecan.com>
2020-06-29 20:49:55 +0900

    ...

--- glossary.js
+++ glossary.js
@@ -2,6 +2,7 @@ var glossary = {
     'delete': '(消す|消去する|削除する)',
     'display': '(表示する|見る|リストする)',
     'change': '(変更|修正|編集|更新)',
-    'modified': '(変更|修正|編集|更新)(された|があった)'
+    'modified': '(変更|修正|編集|更新)(された|があった)',
+    'compare': '(比較する|比べる)'
 }
 module.exports = glossary
