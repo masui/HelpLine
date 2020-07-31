7855c321be89a23c964a277446adf5ea1bbedd9c
Toshiyuki Masui <masui@pitecan.com>
2020-07-31 14:55:52 +0900

    プロジェクトリストページで起動すると全ページを調べるようにした

--- omnibox/content_script.js
+++ omnibox/content_script.js
@@ -4,6 +4,52 @@ require("re_expand") // browserifyで展開
 //chrome.storage.local.set({'suggests': {}}, function () {
 //});
 //return;
+
+var suggests = {}
+var descs = []
+
+function terminate_def(cmd){
+    if(descs.length > 0){
+	for(l of descs){
+	    var m = l.match(/^\?\s+(.*)/)
+	    expanded = m[1].expand() // Helpfeel記法の正規表現を展開
+	    for(s of expanded){
+		suggests[s] = cmd
+	    }
+	}
+	chrome.storage.sync.set({'suggests': suggests}, function(){ });
+    }
+    descs = []
+}
+
+function process(lines,project,title){
+    //
+    // Scrapboxページの内容を1行ずつ調べてHelpfeel記法を処理する
+    //
+    descs = [] // Helpfeel記法
+    for(var line of lines){
+	if(line.match(/^\?\s/)){ // ? ではじまるHelpfeel記法
+	    descs.push(line)
+	}
+	else if(line.match(/^\%\s/)){ // % ではじまるコマンド指定
+	    if(descs.length == 0){
+		alert("Helpfeel記法が定義されていません")
+	    }
+	    else {
+		m = line.match(/^\%\s+(echo|open)\s+(.*)/)
+		if(m){
+		    terminate_def(m[2])
+		}
+		descs = []
+	    }
+
+	}
+	else {
+	    terminate_def(`https://scrapbox.io/${project}/${title}`)
+	}
+    }
+    terminate_def(`https://scrapbox.io/${project}/${title}`)
+}
 	
 //
 // コールバックでpopup.jsからの値を受け取る
@@ -13,84 +59,42 @@ chrome.runtime.onMessage.addListener(message => {
 	return;
     }
     chrome.storage.sync.get(["suggests"], function (value) {
-	var suggests = {}
 	if(value.suggests){
 	    suggests = value.suggests
 	}
 	
-	m = location.href.match(/scrapbox\.io\/([a-zA-Z0-9\-]+)\/?$/i)
-	if(m){
-	    project = m[1]
-	    alert(project)
-	    fetch(`https://scrapbox.io/api/pages/${project}?limit=10`)
-		.then(function(response) {
-		    return response.json();
-		})
-		.then(function(json) {
-		    for(var page of json.pages){
-			alert(page.title)
-			fetch(`https://scrapbox.io/api/pages/${project}/${page.title}/text`)
-			    .then(function(response) {
-				return response.text()
-			    })
-			    .then(function(text){
-				alert(text)
-			    })
-  		    }
-		});
-	}
-	else {
-	    //
-	    // Scrapboxページの内容を1行ずつ調べてHelpfeel記法を処理する
-	    //
-	    descs = [] // Helpfeel記法
-	    for(line of document.querySelectorAll('code')){
-		if(line.className == "helpfeel"){ // ? ではじまるHelpfeel記法
-		    descs.push(line)
-		}
-		else if(line.className == "cli"){ // % ではじまるコマンド指定
-		    if(descs.length == 0){
-			alert("Helpfeel記法が定義されていません")
-		    }
-		    else {
-			m = line.textContent.match(/^\%\s+(echo|open)\s+(.*)/)
-			cmd = m[2]
-			for(l of descs){
-			    m = l.textContent.match(/^\?\s+(.*)/)
-			    expanded = m[1].expand() // Helpfeel記法の正規表現を展開
-			    for(s of expanded){
-				suggests[s] = cmd
-			    }
-			}
-			chrome.storage.sync.set({'suggests': suggests}, function(){ });
-		    }
-		    descs = []
-		}
-		else {
-		    if(descs.length > 0){
-			for(l of descs){
-			    m = l.textContent.match(/^\?\s+(.*)/)
-			    expanded = m[1].expand()
-			    for(s of expanded){
-				suggests[s] = location.href
-			    }
-			}
-			chrome.storage.sync.set({'suggests': suggests}, function(){ });
-		    }
-		    descs = []
-		}
+	m = location.href.match(/scrapbox\.io\/([a-zA-Z0-9\-]+)(\/(.*))?$/)
+	if(m[1]){
+	    var project = m[1]
+	    var title = m[3]
+	    if(!title){ // ページリスト
+		fetch(`https://scrapbox.io/api/pages/${project}?limit=100`)
+		    .then(function(response) {
+			return response.json();
+		    })
+		    .then(function(json) {
+			for(var page of json.pages){
+			    var title = page.title
+			    console.log(title)
+			    fetch(`https://scrapbox.io/api/pages/${project}/${title}/text`)
+				.then(function(response) {
+				    return response.text()
+				})
+				.then(function(text){
+				    process(text.split(/\n/),project,title)
+				})
+  			}
+		    });
 	    }
-	    if(descs.length > 0){
-		for(l of descs){
-		    m = l.textContent.match(/^\?\s+(.*)/)
-		    expanded = m[1].expand()
-		    for(s of expanded){
-			suggests[s] = location.href
-		    }
-		}
-		chrome.storage.sync.set({'suggests': suggests}, function(){ });
+	    else { // 単独ページ
+		fetch(`https://scrapbox.io/api/pages/${project}/${title}/text`)
+		    .then(function(response) {
+			return response.text()
+		    })
+		    .then(function(text){
+			process(text.split(/\n/),project,title)
+		    })
 	    }
 	}
     })
 });
-				     
