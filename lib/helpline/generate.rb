63da8524e8017c62aab5a33001dc288d86721128
Toshiyuki Masui <masui@pitecan.com>
2020-10-09 17:44:49 +0900

    generate.rbは実験中...

--- lib/helpline/generate.rb
+++ lib/helpline/generate.rb
@@ -22,7 +22,7 @@ class HelpLine
     #
     # HelpLineエントリ
     #
-
+    # debug = true
     logfile = (debug ? "/tmp/helpline-defs" : "/dev/null")
     File.open(logfile,"w"){ |f|  # ログを残す場合
       entries = []
@@ -35,7 +35,7 @@ class HelpLine
             desc = eval('"' + l + '"')
             f.puts "desc: #{desc}"
             g.add desc.force_encoding('utf-8'), cmd.force_encoding('utf-8')
-            # puts "g.add(\"#{desc.force_encoding('utf-8')}\",\"#{cmd.force_encoding('utf-8')}\")"
+            # f.puts "g.add(\"#{desc.force_encoding('utf-8')}\",\"#{cmd.force_encoding('utf-8')}\")"
           }
           f.puts "cmd:  #{cmd}"
           entries = []
@@ -43,14 +43,24 @@ class HelpLine
       }
     }
 
+    # generateは曖昧検索も行なう => res[0], res[1], res[2] にそれぞれ入る
     res = g.generate query
 
     File.open("/tmp/helpline.cmd","w"){ |f|
+      f.puts "query = <#{query}>"
+      f.puts "--------------------"
       f.puts ARGV.join(' ')
+      f.puts "--------------------"
+      f.puts res[0]
+      f.puts "--------------------"
+      f.puts res[1]
+      f.puts "--------------------"
+      f.puts res[2]
     }
 
     listed = {}
-    list = res[0].find_all { |a| # 0 ambig
+    # list = res[0].find_all { |a| # 0 ambig
+    list = res[1].find_all { |a| # 1 ambig
       # a = ["git: 現在の状況を表示する {56}", "git status {56}"], etc.
       if a[0] =~ /voidvoidvoid/
         false
